741549f6b7ccf4ff9355218aee98773448855f79
kmazza-hub <kmazza2006@yahoo.com>
2025-04-10 20:09:16 -0400

    updated errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -1,5 +1,16 @@
+// utils/errors.js
+
+// Define common HTTP status codes
+const INTERNAL_SERVER_ERROR = 500;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
+const NOT_FOUND = 404;
+
 module.exports = {
-  BAD_REQUEST: 400,
-  CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500,
+  INTERNAL_SERVER_ERROR,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  FORBIDDEN,
+  NOT_FOUND
 };
